dea1b27913d540eb68c9fa9f762517f0c8d121bd
MarinaMeza <kaiodelnorte@yahoo.com.ar>
2015-09-02 09:03:14 -0300

    comentarios y prueba

---
+++ TxT.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _15_08_26
+{
+    class TxT
+    {
+        // CONSTRUCTOR:
+         //Los Constructores no retorna nada "void"
+         //Pede ser publico o privado
+         //se llama igual q la clase
+        /**/
+        //el new Motor();
+         //Usa el Constructor de la clase motor
+        /**/
+        //Asignacion antes de los Constructores:
+        //MiAuto.MotorDelAuto = new Motor();
+        //MiAuto.Rueda1 = new Neumatico();
+        //MiAuto.Rueda2 = new Neumatico();
+        //MiAuto.Rueda3 = new Neumatico();
+        //MiAuto.Rueda4 = new Neumatico();
+        //
+        //MiAuto.marca = "Audi";
+        //MiAuto.KilometrosRecorridos = 0;   // KilometrosRecorridos Paso a ser private
+        // 
+        //MiAuto.MotorDelAuto.marca = "Audi";
+        //MiAuto.MotorDelAuto.serviceKm = 50000;           
+        //
+        //MiAuto.Rueda1.marca = "Audi";
+        //MiAuto.Rueda1.cambioKm = 25000;
+        //MiAuto.Rueda2.marca = "Audi";
+        //MiAuto.Rueda2.cambioKm = 25000;
+        //MiAuto.Rueda3.marca = "Audi";
+        //MiAuto.Rueda3.cambioKm = 25000;
+        //MiAuto.Rueda4.marca = "Audi";
+        //MiAuto.Rueda4.cambioKm = 25000;
+        /**/
+        //sobrecarga:
+        //nombrar de la misma manera al metodo y cambiarle la firma al metodo 
+        //(modificando el tipo de parametro y el orden en el que recibe los parametros)
+
+    }
+}
